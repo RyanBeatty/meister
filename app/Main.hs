e467aa42ac84befcd91dde448103a5b76d025541
Ryan Beatty <rvbeatty@email.wm.edu>
2018-05-20 22:33:33 -0700

    initial framework with word count example commited

--- app/Main.hs
+++ app/Main.hs
@@ -2,6 +2,17 @@ module Main where
 
 import Meister
 
+import qualified Data.Text as T (Text, words)
+import           Data.Word      (Word64)
+
+mapper :: FilePath -> T.Text -> [(T.Text, Word64)]
+mapper _ text = flip (,) 1 <$> T.words text
+
+reducer :: T.Text -> [Word64] -> (T.Text, Word64)
+reducer w occurrences = (w, sum occurrences)
+
 main :: IO ()
 main = do
-    print $ "result: " ++ (show $ mapReduce [1, 2, 3, 4] (+1))
+    print "Counting Words"
+    mapReduce "package.yaml" "out.txt" mapper reducer 
+    return ()
--- src/Meister.hs
+++ src/Meister.hs
@@ -2,7 +2,21 @@ module Meister
     ( mapReduce
     ) where
 
-import Data.Functor
+import           Data.Functor
+import qualified Data.Map.Strict as M   (empty, insertWith, toList)  
+import qualified Data.Text       as T   (Text)
+import qualified Data.Text.IO    as TIO (readFile, writeFile)
 
-mapReduce :: [a] -> (a -> b) -> [b]
-mapReduce items f = f <$> items
+type Mapper c d      = (FilePath -> T.Text -> [(c, d)])
+type Reducer c d e f = (c -> [d] -> (e, f))
+
+mapReduce :: (Ord c, Show c, Show d, Show e, Show f) => FilePath -> FilePath -> Mapper c d -> Reducer c d e f -> IO ()
+mapReduce infile outfile mapper reducer = do
+    -- TODO: Assuming file is utf-8 encoded.
+    -- TODO: Catch file not found error?
+    input <- TIO.readFile infile
+    let mapped    = mapper infile input
+    let collected = M.toList $ foldr (\(c, d) hm -> M.insertWith (++) c [d] hm) M.empty mapped
+    let reduced   = uncurry reducer <$> collected
+    print reduced
+    return ()
